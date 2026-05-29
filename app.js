7d7fae42d6432f249dd0eeec0a0192fbb2e9027b
danielgr450 <gonzalezrosasd5@gmail.com>
2026-05-28 21:02:40 -0600

    Agregue tema y claro

--- app.js
+++ app.js
@@ -85,6 +85,23 @@ function initMobileMenu() {
             link.classList.add("active");
         });
     });
+    const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+
+// Tema inicial
+body.classList.add('dark-theme');
+
+themeToggle.addEventListener('click', () => {
+
+    if(body.classList.contains('dark-theme')) {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+    } else {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+    }
+
+});
     
 
     // Highlight navbar menu item on scroll (Simple Scrollspy)
